5956593be572798a93c0af697357c00c299522d6
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2025-01-02 18:54:09 +0530

    red theme done

--- src/styles/mypreset.ts
+++ src/styles/mypreset.ts
@@ -4,19 +4,34 @@ import Aura from '@primeng/themes/aura';
 
 const MyPreset = definePreset(Aura, {
     //Your customizations, see the following sections for examples
+    // semantic: {
+    //     primary: {
+    //         50: '{indigo.50}',
+    //         100: '{indigo.100}',
+    //         200: '{indigo.200}',
+    //         300: '{indigo.300}',
+    //         400: '{indigo.400}',
+    //         500: '{indigo.500}',
+    //         600: '{indigo.600}',
+    //         700: '{indigo.700}',
+    //         800: '{indigo.800}',
+    //         900: '{indigo.900}',
+    //         950: '{indigo.950}'
+    //     }
+    // }
     semantic: {
         primary: {
-            50: '{indigo.50}',
-            100: '{indigo.100}',
-            200: '{indigo.200}',
-            300: '{indigo.300}',
-            400: '{indigo.400}',
-            500: '{indigo.500}',
-            600: '{indigo.600}',
-            700: '{indigo.700}',
-            800: '{indigo.800}',
-            900: '{indigo.900}',
-            950: '{indigo.950}'
+            50: '{red.50}',
+            100: '{red.100}',
+            200: '{red.200}',
+            300: '{red.300}',
+            400: '{red.400}',
+            500: '{red.500}',
+            600: '{red.600}',
+            700: '{red.700}',
+            800: '{red.800}',
+            900: '{red.900}',
+            950: '{red.950}'
         }
     }
 });
