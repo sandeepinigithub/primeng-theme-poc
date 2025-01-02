34e8960d727b734127b05b106638231817b0449c
Sandeep Shukla <sshukla@supplyvalid.com>
2025-01-02 17:10:48 +0530

    initial check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc-primeng';
+  products = [
+    { name: 'Laptop', price: 1500 },
+    { name: 'Phone', price: 800 },
+    { name: 'Tablet', price: 600 }
+  ];
+
+
+  cities: any[] = [
+    {name: 'New York', code: 'NY'},
+    {name: 'Rome', code: 'RM'},
+    {name: 'London', code: 'LDN'},
+    {name: 'Istanbul', code: 'IST'},
+    {name: 'Paris', code: 'PRS'}
+];;
+
+  selectedCities:any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    TableModule,
+    MultiSelectModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
